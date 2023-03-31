da8401e8ec972e1f9deb7827caa53b98755ca842
Jaspreet Singh <66967130+jaspreet305@users.noreply.github.com>
2023-03-31 15:19:31 -0400

    almost finish pet profile

--- frontend/src/components/Profile/PetProfile.js
+++ frontend/src/components/Profile/PetProfile.js
@@ -1,11 +1,64 @@
 import "./PetProfile.css";
 import * as React from "react";
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export function PetProfile() {
 
     return (
-        <>
-
-        </>
+        <Container className="profile-page-container">
+            <Grid className="pet-picture">
+            </Grid>
+            <Grid className="pet-info">
+                <div className="pet-name">
+                    <h1>Beast</h1>
+                </div>
+                <div className="pet-breed">
+                    <h2>Shih Tzu</h2>
+                </div>
+                <div className="info-row">
+                    <Grid className="specific-info">
+                        <h3>AGE</h3>
+                        <p>12 years</p>
+                    </Grid>
+                    <Grid className="specific-info">
+                        <h3>HEALTH</h3>
+                        <p>Fit</p>
+                    </Grid>
+                    <Grid className="specific-info">
+                        <h3>SEX</h3>
+                        <p>Male</p>
+                    </Grid>
+                </div>
+                <div className="info-row">
+                    <Grid>
+                        <h3>LOCATION</h3>
+                        <p>Montreal, Quebec</p>
+                    </Grid>
+                </div>
+                <div className="info-row">
+                    <Grid>
+                        <h3>DETAILS</h3>
+                        <p>Beast enjoys the company of humans. He can sometimes be aggressive around other dogs, especially chihuahuas. Also, he will get really mad if you try to treat him like a child. He will like you if you respect him.</p>
+                    </Grid>
+                </div>
+                <div className="info-row">
+                    <Grid>
+                        <h3>OWNER</h3>
+                        <div className="owner">
+                            <AccountCircleIcon className="owner-icon" />
+                            <p> Noah Assayag </p>
+                        </div>
+                    </Grid>
+                </div>
+                <div>
+                    <Button className="contact-owner-button" variant="contained" >
+                        Contact Owner
+                    </Button>
+                </div>
+            </Grid>
+        </Container>
     );
 }
