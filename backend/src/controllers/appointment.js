4c6b38a21cd296e1b97397412531dcb391e6a926
Amin Boulemkahel <aminboulemkahel@gmail.com>
2023-04-08 12:53:11 -0400

    edit worker, pet and appointment

---
+++ backend/src/controllers/appointment.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const appointmentService = require("../services/appointment");
+const auth = require("../middlewares/auth");
+
+router.post('/', auth, async (req, res) => {
+    try {
+        let appointment = req.body;
+        const result = await appointmentService.createAppointment(appointment);
+        res.status(result.status).send(result.data);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const result = await appointmentService.cancelAppointment(req.params.id);
+        res.status(result.status).send(result.data);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.put('/:id', auth, async (req, res) => {
+    try {
+        let appointment = req.body;
+        const result = await appointmentService.editAppointment(req.params.id, appointment.date);
+        res.status(result.status).send(result.data);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+module.exports = router;
--- backend/src/services/appointment.js
+++ backend/src/services/appointment.js
@@ -1,5 +1,5 @@
 const {Worker, validate} = require("../models/worker");
-const {BadRequest, NotFound, Created} = require("../utils/results");
+const {BadRequest, NotFound, Created, Success} = require("../utils/results");
 const {Appointment, validate} = require("../models/appointment");
 const sgMail = require('@sendgrid/mail');
 const {User} = require("../models/user");
@@ -23,7 +23,7 @@ const createAppointment = async (appointment) => {
     // Check if the worker can accept the appointment based on its type
     if (
         (appointment.type === "training" && worker.type !== "trainer") ||
-        (appointment.type === "grooming" && worker.type !== "groomer")||
+        (appointment.type === "grooming" && worker.type !== "groomer") ||
         (appointment.type === "sitting" && worker.type !== "sitter")
     ) return BadRequest("Invalid appointment type for this worker");
 
@@ -34,4 +34,85 @@ const createAppointment = async (appointment) => {
     return Created(app);
 };
 
+const cancelAppointment = async (appointmentId) => {
+    if (!appointmentId)
+        return BadRequest("Appointment id not found");
+
+    const deletedAppointment = await Appointment.findByIdAndDelete(appointmentId);
+
+    if (!deletedAppointment)
+        return BadRequest("Appointment not found");
+
+    // Send email to user
+    const userEmail = deletedAppointment.user.email;
+    const petName = deletedAppointment.pet.name;
+    const appointmentDate = deletedAppointment.date.toDateString();
+    const emailBody = `Your appointment for ${deletedAppointment.type} on ${appointmentDate} for ${petName} has been cancelled. We apologize for any inconvenience caused.`;
+    const userMsg = {
+        to: userEmail,
+        from: process.env.SENDGRID_EMAIL,
+        subject: 'Appointment Cancelled',
+        text: emailBody,
+        html: `<p>${emailBody}</p>`,
+    };
+    await sgMail.send(userMsg);
+
+    // Send email to worker
+    const workerEmail = deletedAppointment.worker.email;
+    const workerBody = `The appointment scheduled for ${deletedAppointment.type} on ${appointmentDate} with ${deletedAppointment.user.firstName} ${deletedAppointment.user.lastName}'s pet ${petName} has been cancelled.`;
+    const workerMsg = {
+        to: workerEmail,
+        from: process.env.SENDGRID_EMAIL,
+        subject: 'Appointment Cancelled',
+        text: workerBody,
+        html: `<p>${workerBody}</p>`,
+    };
+    await sgMail.send(workerMsg);
+
+    return Success(deletedAppointment._id);
+};
+
+const editAppointment = async (appointmentId, date) => {
+    if (!appointmentId)
+        return BadRequest("Appointment id not found");
+
+    let a = await Appointment.findById(appointmentId);
+    a.date = date;
+
+    a = await Appointment.findById(appointmentId, a, {new: true});
+
+    if (!a)
+        return NotFound("Error while updating the appointment");
+
+    // Send email to user
+    const userEmail = a.user.email;
+    const petName = a.pet.name;
+    const appointmentDate = a.date.toDateString();
+    const emailBody = `Your appointment for ${a.type} has been rescheduled to ${appointmentDate} for ${petName}. Thank you for choosing our service!`;
+    const userMsg = {
+        to: userEmail,
+        from: process.env.SENDGRID_EMAIL,
+        subject: 'Appointment Rescheduled',
+        text: emailBody,
+        html: `<p>${emailBody}</p>`,
+    };
+    await sgMail.send(userMsg);
+
+    // Send email to worker
+    const workerEmail = a.worker.email;
+    const workerBody = `The appointment scheduled for ${a.type} on ${appointmentDate} with ${a.user.firstName} ${a.user.lastName}'s pet ${petName} has been rescheduled.`;
+    const workerMsg = {
+        to: workerEmail,
+        from: process.env.SENDGRID_EMAIL,
+        subject: 'Appointment Rescheduled',
+        text: workerBody,
+        html: `<p>${workerBody}</p>`,
+    };
+    await sgMail.send(workerMsg);
+
+    return Success(a);
+};
+
 exports.createAppointment = createAppointment;
+exports.cancelAppointment = cancelAppointment;
+exports.editAppointment = editAppointment;
