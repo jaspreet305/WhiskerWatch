9f2214df658b547b5c9f320ebddc56a91e5dfd58
Anis Brachemi <a_brache@live.concordia.ca>
2023-04-13 11:53:05 -0400

    Pet Provider Search Started

--- frontend/src/components/Search/PetProviderSearch.js
+++ frontend/src/components/Search/PetProviderSearch.js
@@ -11,50 +11,79 @@ export const PetProviderSearch = () => {
           <div className={"pet-provider-search-search"}>Search</div>
           <img className={"pet-provider-search-icon-search"} src={"/img/search.svg"} />
         </div>
-        <div className={"pet-provider-search-overlap-group5"}>
-          <div className={"pet-provider-search-text-wrapper"}>Sitters</div>
+        <div className={"pet-provider-search-overlap-buttons"}>
+          <div className={"pet-provider-search-overlap-selected"}>
+            <div className={"pet-provider-search-text-wrapper"}>Sitters</div>
+          </div>
+          <div className={"pet-provider-search-overlap-unselected"}>
+            <div className={"pet-provider-search-text-wrapper"}>Trainers</div>
+          </div>
+          <div className={"pet-provider-search-overlap-unselected"}>
+            <div className={"pet-provider-search-text-wrapper"}>Groomers</div>
+          </div>
         </div>
-        <div className={"pet-provider-search-overlap-group2"}>
-          <div className={"pet-provider-search-text-wrapper-2"}>Trainers</div>
+        <div className={"pet-provider-container"}>
+          <div className={"pet-provider-search-overlap-group"}>
+            <div className={"pet-provider-search-text-wrapper-4"}>Noah</div>
+            <div className={"pet-provider-search-dog-sitter"}> Dog Sitter</div>
+            <div className={"pet-provider-search-age"}>Age</div>
+            <div className={"pet-provider-search-text-wrapper-5"}>Cost</div>
+            <div className={"pet-provider-search-element-stars"}>5 Stars</div>
+            <div className={"pet-provider-search-element-yrs"}>21 Yrs</div>
+            <div className={"pet-provider-search-text-wrapper-6"}>Rating</div>
+            <div className={"pet-provider-search-text-wrapper-7"}>Expertise</div>
+            <div className={"pet-provider-search-text-wrapper-8"}>Male</div>
+            <div className={"pet-provider-search-text-wrapper-9"}>28$/hour</div>
+            <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
+            <div className={"pet-provider-search-rectangle-14"} />
+            <div className={"pet-provider-search-text-wrapper-10"}>Noah Assayag</div>
+            <div className={"pet-provider-search-dog-sitter-2"}> Dog Sitter</div>
+            <div className={"pet-provider-search-AGE"}>Age</div>
+            <div className={"pet-provider-search-RATE"}>Rate</div>
+            <div className={"pet-provider-search-element-5-stars"}>5 Stars</div>
+            <div className={"pet-provider-search-element-21-yrs"}>21 Yrs</div>
+            <div className={"pet-provider-search-RATING"}>Rating</div>
+            <div className={"pet-provider-search-EXPERTISE"}>Expertise</div>
+            <div className={"pet-provider-search-text-wrapper-11"}>$28/hour</div>
+            <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
+          </div>
+          <div className={"pet-provider-search-overlap-group"}>
+            <div className={"pet-provider-search-text-wrapper-4"}>Noah</div>
+            <div className={"pet-provider-search-dog-sitter"}> Dog Sitter</div>
+            <div className={"pet-provider-search-age"}>Age</div>
+            <div className={"pet-provider-search-text-wrapper-5"}>Cost</div>
+            <div className={"pet-provider-search-element-stars"}>5 Stars</div>
+            <div className={"pet-provider-search-element-yrs"}>21 Yrs</div>
+            <div className={"pet-provider-search-text-wrapper-6"}>Rating</div>
+            <div className={"pet-provider-search-text-wrapper-7"}>Expertise</div>
+            <div className={"pet-provider-search-text-wrapper-8"}>Male</div>
+            <div className={"pet-provider-search-text-wrapper-9"}>28$/hour</div>
+            <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
+            <div className={"pet-provider-search-rectangle-14"} />
+            <div className={"pet-provider-search-text-wrapper-10"}>Noah Assayag</div>
+            <div className={"pet-provider-search-dog-sitter-2"}> Dog Sitter</div>
+            <div className={"pet-provider-search-AGE"}>Age</div>
+            <div className={"pet-provider-search-RATE"}>Rate</div>
+            <div className={"pet-provider-search-element-5-stars"}>5 Stars</div>
+            <div className={"pet-provider-search-element-21-yrs"}>21 Yrs</div>
+            <div className={"pet-provider-search-RATING"}>Rating</div>
+            <div className={"pet-provider-search-EXPERTISE"}>Expertise</div>
+            <div className={"pet-provider-search-text-wrapper-11"}>$28/hour</div>
+            <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
+          </div>
         </div>
-        <div className={"pet-provider-search-overlap-group4"}>
-          <div className={"pet-provider-search-text-wrapper-3"}>Groomers</div>
-        </div>
-        <div className={"pet-provider-search-overlap-group"}>
-          <div className={"pet-provider-search-text-wrapper-4"}>Noah</div>
-          <div className={"pet-provider-search-dog-sitter"}> Dog Sitter</div>
-          <div className={"pet-provider-search-age"}>Age</div>
-          <div className={"pet-provider-search-text-wrapper-5"}>Cost</div>
-          <div className={"pet-provider-search-element-stars"}>5 Stars</div>
-          <div className={"pet-provider-search-element-yrs"}>21 Yrs</div>
-          <div className={"pet-provider-search-text-wrapper-6"}>Rating</div>
-          <div className={"pet-provider-search-text-wrapper-7"}>Expertise</div>
-          <div className={"pet-provider-search-text-wrapper-8"}>Male</div>
-          <div className={"pet-provider-search-text-wrapper-9"}>28$/hour</div>
-          <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
-          <div className={"pet-provider-search-rectangle-14"} />
-          <div className={"pet-provider-search-text-wrapper-10"}>Noah Assayag</div>
-          <div className={"pet-provider-search-dog-sitter-2"}> Dog Sitter</div>
-          <div className={"pet-provider-search-AGE"}>Age</div>
-          <div className={"pet-provider-search-RATE"}>Rate</div>
-          <div className={"pet-provider-search-element-5-stars"}>5 Stars</div>
-          <div className={"pet-provider-search-element-21-yrs"}>21 Yrs</div>
-          <div className={"pet-provider-search-RATING"}>Rating</div>
-          <div className={"pet-provider-search-EXPERTISE"}>Expertise</div>
-          <div className={"pet-provider-search-text-wrapper-11"}>$28/hour</div>
-          <img className={"pet-provider-search-image-removebg-preview"} src={"/img/image-removebg-preview-1.png"} />
-        </div>
-        <div className={"pet-provider-search-overlap-group1"}>
-          <div className={"pet-provider-search-text-wrapper-12"}>Emma Amar</div>
-          <div className={"pet-provider-search-dog-sitter-3"}> Dog Sitter</div>
-          <div className={"pet-provider-search-AGE-2"}>Age</div>
-          <div className={"pet-provider-search-RATE-2"}>Rate</div>
-          <div className={"pet-provider-search-element-stars-2"}>5 Stars</div>
-          <div className={"pet-provider-search-element-yrs-2"}>21 Yrs</div>
-          <div className={"pet-provider-search-RATING-2"}>Rating</div>
-          <div className={"pet-provider-search-EXPERTISE-2"}>Expertise</div>
-          <div className={"pet-provider-search-text-wrapper-13"}>$32/hour</div>
-        </div>
+
+        {/*<div className={"pet-provider-search-overlap-group1"}>*/}
+        {/*  <div className={"pet-provider-search-text-wrapper-12"}>Emma Amar</div>*/}
+        {/*  <div className={"pet-provider-search-dog-sitter-3"}> Dog Sitter</div>*/}
+        {/*  <div className={"pet-provider-search-AGE-2"}>Age</div>*/}
+        {/*  <div className={"pet-provider-search-RATE-2"}>Rate</div>*/}
+        {/*  <div className={"pet-provider-search-element-stars-2"}>5 Stars</div>*/}
+        {/*  <div className={"pet-provider-search-element-yrs-2"}>21 Yrs</div>*/}
+        {/*  <div className={"pet-provider-search-RATING-2"}>Rating</div>*/}
+        {/*  <div className={"pet-provider-search-EXPERTISE-2"}>Expertise</div>*/}
+        {/*  <div className={"pet-provider-search-text-wrapper-13"}>$32/hour</div>*/}
+        {/*</div>*/}
         <img className={"pet-provider-search-left-side"} src={"/img/left-side.png"} />
         <div className={"pet-provider-search-right-side"}>
           <img className={"pet-provider-search-battery"} src={"/img/battery-5.png"} />
