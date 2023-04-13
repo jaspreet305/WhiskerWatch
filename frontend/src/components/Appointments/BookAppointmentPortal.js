481683839eb6d14e44c61df04417b16587726169
emma-amar <emma.h.amar@example.com>
2023-04-13 16:33:48 -0400

    calendar fixed to be responsive and time selector

--- frontend/src/components/Appointments/BookAppointmentPortal.js
+++ frontend/src/components/Appointments/BookAppointmentPortal.js
@@ -1,184 +1,84 @@
 import React from "react";
 import "./BookAppointmentPortal.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Select from 'react-select';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export const BookAppointmentPortal = () => {
+  const handleDateChange = (date) => {
+    console.log(date);
+  };
+
+  const timeOptions = [
+    { value: '09:00', label: '09:00 AM' },
+    { value: '09:30', label: '09:30 AM' },
+    { value: '10:00', label: '10:00 AM' },
+  ];
+  
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const handleTimeChange = (selectedOption) => {
+    setSelectedTime(selectedOption);
+    console.log(`Selected time:`, selectedOption);
+  };
+
+  const navigate = useNavigate();
+
+  const handleArrowClick = () => {
+    navigate("/search");
+  };
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      height: '50px',
+      borderRadius: '8px',
+      border: state.isFocused ? '1px solid #F8D1CB' : '1px solid #E5E5E5',
+      boxShadow: state.isFocused ? '0 0 4px #F8D1CB' : 'none',
+      '&:hover': {
+        border: state.isFocused ? '1px solid #F8D1CB' : '1px solid #E5E5E5',
+      }
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#F8D1CB' : 'white',
+      color: state.isSelected ? 'white' : '#495057',
+      '&:hover': {
+        backgroundColor: '#F8D1CB',
+        color: 'white',
+      },
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#495057',
+    }),
+  };
+  
   return (
     <div className={"book-appointment-portal-div"}>
       <div className={"book-appointment-portal-book-appointment-portal"}>
-        <div className={"book-appointment-portal-overlap"}>
+        <div className={"book-appointment-portal-overlap"} onClick={handleArrowClick}>
           <h1 className={"book-appointment-portal-explore"}>Explore</h1>
           <img className={"book-appointment-portal-arrow"} src={"/img/arrow-1-1.svg"} />
           <img className={"book-appointment-portal-rectangle"} src={"/img/rectangle-26-1.svg"} />
-          <img className={"book-appointment-portal-arrow-1"} src={"/img/arrow-1-1.svg"} />
+          <img className={"appointments-arrow"} src={"/img/arrow-1-1.svg"} />
         </div>
         <div className={"book-appointment-portal-overlap-group4"}>
           <div className={"book-appointment-portal-text-wrapper"}>Date</div>
           <div className={"book-appointment-portal-calendar-april"}>
-            <div className={"book-appointment-portal-dating-month-label"}>
-              <div className={"book-appointment-portal-text-wrapper-2"}>April</div>
-            </div>
-            <div className={"book-appointment-portal-frame"}>
-              <article className={"book-appointment-portal-dating-week-label"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>SUN</div>
-              </article>
-              <article className={"book-appointment-portal-SUN-wrapper"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>MON</div>
-              </article>
-              <article className={"book-appointment-portal-div-wrapper"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>TUE</div>
-              </article>
-              <article className={"book-appointment-portal-article"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>WED</div>
-              </article>
-              <article className={"book-appointment-portal-dating-week-label-2"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>THU</div>
-              </article>
-              <article className={"book-appointment-portal-dating-week-label-3"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>FRI</div>
-              </article>
-              <article className={"book-appointment-portal-dating-week-label-4"}>
-                <div className={"book-appointment-portal-text-wrapper-3"}>SAT</div>
-              </article>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>23</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>24</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>25</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>26</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>27</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>28</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>1</div>
-              </div>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>2</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>3</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>4</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>5</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>6</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>7</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>8</div>
-              </article>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>9</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>10</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>11</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>12</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>13</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>14</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>15</div>
-              </article>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>16</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>17</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>18</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>19</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>20</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>21</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>22</div>
-              </article>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>23</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>24</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>25</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>26</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>27</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>28</div>
-              </article>
-              <article className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>29</div>
-              </article>
-            </div>
-            <div className={"book-appointment-portal-week"}>
-              <div className={"book-appointment-portal-dating-day-in-month"}>
-                <div className={"book-appointment-portal-num"}>30</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>1</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>2</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>3</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>4</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>5</div>
-              </div>
-              <div className={"book-appointment-portal-dating-day-out-of-month"}>
-                <div className={"book-appointment-portal-num"}>6</div>
-              </div>
-            </div>
+            <DatePicker
+              inline
+              selected={new Date()}
+              onChange={handleDateChange}
+              calendarClassName={"book-appointment-portal-custom-calendar"}
+              style={{ zIndex: 1000 }}
+            />
           </div>
+        </div>
+        <div className={"book-appointment-portal-text-wrapper-7"}>Time
         </div>
         <div className={"book-appointment-portal-text-wrapper-4"}>Appointment Type</div>
         <div className={"book-appointment-portal-overlap-group5"}>
@@ -188,12 +88,21 @@ export const BookAppointmentPortal = () => {
         <div className={"book-appointment-portal-overlap-group2"}>
           <div className={"book-appointment-portal-text-wrapper-6"}>Confirm Appointment</div>
         </div>
-        <div className={"book-appointment-portal-text-wrapper-7"}>Time</div>
-        <div className={"book-appointment-portal-overlap-group1"}>
+        <div className={"book-appointment-portal-overlap-group1"}>         
+        <div className={"book-appointment-portal-time-selection-wrapper"}>
+            <Select
+              value={selectedTime}
+              onChange={handleTimeChange}
+              options={timeOptions}
+              isSearchable={false}
+              styles={customStyles}
+              className={"book-appointment-portal-select"}
+            />
           <img className={"book-appointment-portal-img"} src={"/img/arrow-1-2.svg"} />
-          <div className={"book-appointment-portal-text-wrapper-8"}>10:00 am</div>
+          <div className={"book-appointment-portal-text-wrapper-8"}></div>
         </div>
       </div>
     </div>
-  );
+    </div>
+  );  
 };
